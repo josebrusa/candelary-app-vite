a95647c95db61394c4ed65ad5c621c71448370cb
josebrusa <josebrusa@outlook.com>
2023-10-13 12:03:57 +0200

    inicializacion de la app calendario

---
+++ src/calendar/pages/CalendarPages.jsx
@@ -0,0 +1,3 @@
+export const CalendarPages = () => {
+    return <div>CalendarPages</div>;
+};
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { LoginPages } from "../auth";
+import { CalendarPages } from "../calendar";
+import { Navigate } from "react-router-dom";
+
+export const AppRouter = () => {
+    const authStatus = "no-authenticated";
+    return (
+        <Routes>
+            {authStatus === "no-authenticated" ? (
+                <Route path="/auth/*" element={<LoginPages />} />
+            ) : (
+                <Route path="/*" element={<CalendarPages />} />
+            )}
+
+            <Route path="/*" element={<Navigate to="/auth/login" />} />
+        </Routes>
+    );
+};
