dddb701fa38a423d39e73d3a9ddd3d9ab42981d5
josebrusa <josebrusa@outlook.com>
2023-10-20 11:31:18 +0200

    agregando nuevo evento

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,12 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onSetActiveEvent } from "../store";
+import { onAddNewEvent, onSetActiveEvent } from "../store";
 
 export const useCalendarStore = () => {
     const { events, activeEvent } = useSelector((state) => state.calendar);
     const dispatch = useDispatch();
+
     const setActiveEvent = (calendarEvent) => {
         dispatch(onSetActiveEvent(calendarEvent));
     };
+
+    const startSavingEvent = async (calendarEvent) => {
+        //Todo: llegar al backend
+
+        //todo: ok
+        if (calendarEvent._id) {
+            //Actializando
+        } else {
+            //Creando
+            dispatch(
+                onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() })
+            );
+        }
+    };
+
     return {
         //*Properties
         events,
@@ -14,5 +30,6 @@ export const useCalendarStore = () => {
 
         //*Metods
         setActiveEvent,
+        startSavingEvent,
     };
 };
