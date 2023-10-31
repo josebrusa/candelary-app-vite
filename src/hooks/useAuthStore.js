cc118c21a45420dba7da21c33f522b957e2b4b74
josebrusa <josebrusa@outlook.com>
2023-10-31 12:12:35 +0100

    dispatch onlgout and onlogin and onerrormessage

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -1,16 +1,26 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calendarApi } from "../api";
+import { onChecking, onClearErrorMessage, onLogin, onLogout } from "../store";
 
 export const useAuthStore = () => {
     const { status, user, errorMessage } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
 
     const startLogin = async ({ email, password }) => {
+        dispatch(onChecking());
         try {
-            const resp = await calendarApi.post("/auth", { email, password });
-            console.log({ resp });
+            const { data } = await calendarApi.post("/auth", {
+                email,
+                password,
+            });
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("token-init-date", new Date().getTime());
+            dispatch(onLogin({ name: data.name, uid: data.uid }));
         } catch (error) {
-            console.log({ error });
+            dispatch(onLogout("credenciales incorrectas"));
+            setTimeout(() => {
+                dispatch(onClearErrorMessage());
+            }, 100);
         }
     };
 
